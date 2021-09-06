45ce3128d74bdccbc926a7a577228f00e3ae4cb7
Zuza Brzozowska <zuzabrzozowska@Zuzannas-MacBook-Air.local>
2021-09-06 13:19:16 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react'
+import './reset.css'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Pages/home'
+import Resume from './components/Pages/resume'
+import Projects from './components/Pages/projects'
+import About from './components/Pages/about'
+import Sidebar from './components/parts/sidebar'
 
-function App() {
+// would be cool to add: 
+// * mouse eyes follow
+
+const App = ({ }) => {
+  const [background, setBackground] = useState('');
+  const [showScrollUp, setShowScrollUp] = useState(false)
+  const bgRef = useRef();
+  bgRef.current = background;
+
+
+  useEffect(() => {
+    document.addEventListener('scroll', handleScroll);
+    return () => {
+      document.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+
+  const handleScroll = () => {
+    if (window.scrollY === 0) {
+      setShowScrollUp(false)
+      setBackground('bg-eggshell')
+    }
+    if (window.scrollY > 50) {
+      setShowScrollUp(true)
+      setBackground('bg-beige')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <Router>
+      <div className={`row page ${background}`}>
+
+        <Sidebar />
+
+        <div className="col-9 content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/resume">
+              <Resume />
+            </Route>
+            <Route path="/projects">
+              <Projects />
+            </Route>
+          </Switch>
+        </div>
+
+        <a href="#" className={`back-to-top ${showScrollUp ? 'visible' : 'invisible'}`}>
+          <span className="back-to-top-icon">Scroll up</span>
         </a>
-      </header>
-    </div>
+
+      </div>
+
+    </Router>
+
   );
 }
 
---
+++ src/components/Pages/about.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+
+
+const About = () => {
+
+    return (
+        <p>Ac felis donec et odio pellentesque diam volutpat commodo. Lacus laoreet non curabitur gravida. Nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi. Sit amet massa vitae tortor condimentum lacinia quis vel eros. Enim blandit volutpat maecenas volutpat blandit aliquam. Faucibus a pellentesque sit amet porttitor. Justo donec enim diam vulputate ut pharetra sit amet. Morbi tristique senectus et netus et malesuada fames. Id nibh tortor id aliquet lectus. </p>
+    )
+}
+
+export default About
